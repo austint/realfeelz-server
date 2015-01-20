50829114e8b21e2cc4fa5bdd33b5d8299bb1b75d
Leslie Strauss <leslie.strauss@gmail.com>
2015-01-19 20:18:45 -0800

    Commented out all tag and captcha related code. Captcha is now honepot and not currently using tags. Will return to that in the future.

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
 'use strict';
 
 var express = require('express');
+var pg = require('pg');
 var path = require('path');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var BPromise = require('bluebird');
 var request = BPromise.promisifyAll(require('request'));
 var _ = require('lodash');
-
-// keep heroku server awake - ping every 5 minutes (300000)
-// var http = require('http');
-// setInterval(function() {
-//     http.get("http://realfeelz-server.herokuapp.com");
-// }, 300000);
 
 var app = express();
 var resources = express();
@@ -28,19 +23,19 @@ var Statement = bookshelf.Model.extend({
   tableName: 'statements'
 });
 
-var Tag = bookshelf.Model.extend({
-  tableName: 'tags'
-});
+// var Tag = bookshelf.Model.extend({
+//   tableName: 'tags'
+// });
 
-var StatementTag = bookshelf.Model.extend({
-  tableName: 'statements_tags'
-});
+// var StatementTag = bookshelf.Model.extend({
+//   tableName: 'statements_tags'
+// });
 
 app.set('knex', knex);
 app.set('bookshelf', bookshelf);
 app.set('Statement', Statement);
-app.set('Tag', Tag);
-app.set('StatementTag', StatementTag);
+// app.set('Tag', Tag);
+// app.set('StatementTag', StatementTag);
 
 app.use(bodyParser.json());
 
@@ -61,96 +56,109 @@ api.get('/example', function(req, res) {
 
 api.post('/statements', function(req, res) {
   // how do we create StatementTag things?
-
   // if there is a # in the req.body.statement
   var statement = _.pick(req.body.statement, 'body');
   statement.timeAdded = new Date().toISOString();
 
   var body = statement.body;
-  var tagNames = body.match(/#\w+/g) || [];
+  // var tagNames = body.match(/#\w+/g) || [];
   var response = {};
 
   Statement.forge(statement).save().then(function(statement) {
-    response.statement = statement.toJSON();
-  })
-  .then(function() { return tagNames; }) // start working with tag names
-  .map(function(tagName) { // map the tags & create a new tag one at a time
-    return Tag.forge({ tag: tagName }).save();
-  }, { concurrency: 1 }) // concurrency of 1 is kind of just to make tests work
-  .then(function(tags) {
-    // tags are a bunch of bookshelf Tag instances that we just forged
+      response.statement = statement.toJSON();
+    })
+    // .then(function() {
+    //   return tagNames;
+    // }) // start working with tag names
+    // .map(function(tagName) { // map the tags & create a new tag one at a time
+    //   return Tag.forge({
+    //     tag: tagName
+    //   }).save();
+    // }, {
+    //   concurrency: 1
+    // }) // concurrency of 1 is kind of just to make tests work
+    // .then(function(tags) {
+    //   // tags are a bunch of bookshelf Tag instances that we just forged
 
-    if (tags.length) {
-      response.tags = tags;
-    }
-    return tags;
-  })
-  .map(function(tag) {
-    var statement_id = response.statement.id;
-    var tag_id = tag.id;
-    return StatementTag.forge({
-      statement_id: statement_id,
-      tag_id: tag_id
-    }).save();
-  }, { concurrency: 1 })
-  .then(function() {
-    res.json(response);
-  });
+    //   if (tags.length) {
+    //     response.tags = tags;
+    //   }
+    //   return tags;
+    // })
+    // .map(function(tag) {
+    //   var statement_id = response.statement.id;
+    //   var tag_id = tag.id;
+    //   return StatementTag.forge({
+    //     statement_id: statement_id,
+    //     tag_id: tag_id
+    //   }).save();
+    // }, {
+    //   concurrency: 1
+    // })
+    .then(function() {
+      res.json(response);
+    });
 });
 
 api.get('/statements', function(req, res) {
   Statement.query(function(queryBuilder) {
-    queryBuilder.orderBy('timeAdded', 'desc');
-  })
-  .fetchAll()
-  .then(function(statements) {
-    var parsedStatements = statements.toJSON();
-    res.json({ statements: parsedStatements });
-  });
+      queryBuilder.orderBy('timeAdded', 'desc');
+    })
+    .fetchAll()
+    .then(function(statements) {
+      var parsedStatements = statements.toJSON();
+      res.json({
+        statements: parsedStatements
+      });
+    });
 });
 
-api.get('/tags', function(req, res) {
-  Tag.fetchAll().then(function(tags) {
-    res.json({ tags: tags.toJSON() });
-    console.log(res.json);
-  });
-});
+// api.get('/tags', function(req, res) {
+//   Tag.fetchAll().then(function(tags) {
+//     res.json({
+//       tags: tags.toJSON()
+//     });
+//     console.log(res.json);
+//   });
+// });
 
-api.post('/captcha/verify', function(req, res) {
-  var privatekey = '6Ld0jv0SAAAAABBkmO1tSJJgbYIfMzb9kWyT0q3_';
-  var remoteip = req.connection.remoteAddress;
-  var recaptchaAPIEndpoint;
+// api.post('/captcha/verify', function(req, res) {
+//   var privatekey = '6Ld0jv0SAAAAABBkmO1tSJJgbYIfMzb9kWyT0q3_';
+//   var remoteip = req.connection.remoteAddress;
+//   var recaptchaAPIEndpoint;
 
-  if (env === 'test') {
-    recaptchaAPIEndpoint = 'http://localhost:3333/api';
-  } else {
-    recaptchaAPIEndpoint = 'http://www.google.com/recaptcha/api/verify';
-  };
+//   if (env === 'test') {
+//     recaptchaAPIEndpoint = 'http://localhost:3333/api';
+//   } else {
+//     recaptchaAPIEndpoint = 'http://www.google.com/recaptcha/api/verify';
+//   };
 
-  req.body.privatekey = privatekey;
-  req.body.remoteip = remoteip;
+//   req.body.privatekey = privatekey;
+//   req.body.remoteip = remoteip;
 
-  request.postAsync(recaptchaAPIEndpoint, { form: req.body })
-  .spread(function(response, body) {
-    var success = body.split('\n')[0] === 'true';
-    var responseJSON;
+//   request.postAsync(recaptchaAPIEndpoint, {
+//       form: req.body
+//     })
+//     .spread(function(response, body) {
+//       var success = body.split('\n')[0] === 'true';
+//       var responseJSON;
 
-    if (success) {
-      responseJSON = {
-        result: {
-          verified: true
-        }
-      };
-    } else {
-      responseJSON = {
-        result: {
-          verified: false
-        }
-      };
-    }
-    res.send(responseJSON);
-  });
-});
+//       if (success) {
+//         responseJSON = {
+//           result: {
+//             verified: true
+//           }
+//         };
+//       } else {
+//         responseJSON = {
+//           result: {
+//             verified: false
+//           }
+//         };
+//       }
+//       res.send(responseJSON);
+//     });
+// });
 
 // single-page app routes
 app.use('/api', api);
--- test/test.js
+++ test/test.js
@@ -8,145 +8,148 @@ var knex = app.get('knex');
 
 var BPromise = require('bluebird');
 var Statement = app.get('Statement');
-var Tag = app.get('Tag');
-var StatementTag = app.get('StatementTag');
+// var Tag = app.get('Tag');
+// var StatementTag = app.get('StatementTag');
 var tk = require('timekeeper');
 var time = new Date(1415397594052);
 
 require('bluebird').longStackTraces();
 
 describe('The app API.', __app(function() {
-	beforeEach(function(done) {
-		knex('statements_tags').delete()
-    .then(function() {
-      return knex('tags').delete();
-    })
-    .then(function() {
-      return knex('statements').delete();
-    })
-    .then(function() {
-			return knex.raw('alter sequence statements_id_seq restart');
-		})
-    .then(function() {
-      return knex.raw('alter sequence tags_id_seq restart');
-    })
-    .then(function() {
-      return knex.raw('alter sequence statements_tags_id_seq restart');
-    })
-    .then(function() {
-      tk.freeze(time);
-    })
-    .then(function() { done(); }, done);
-	});
+  beforeEach(function(done) {
+    knex('statements').delete()
+    // knex('statements_tags').delete()
+      // .then(function() {
+      //   return knex('tags').delete();
+      // })
+      .then(function() {
+        return knex('statements').delete();
+      })
+      .then(function() {
+        return knex.raw('alter sequence statements_id_seq restart');
+      })
+      .then(function() {
+        return knex.raw('alter sequence tags_id_seq restart');
+      })
+      .then(function() {
+        return knex.raw('alter sequence statements_tags_id_seq restart');
+      })
+      .then(function() {
+        tk.freeze(time);
+      })
+      .then(function() {
+        done();
+      }, done);
+  });
 
   afterEach(function(done) {
     tk.reset();
     done();
   });
 
-	it('handles POST /api/statements', function(done) {
-		this.testAPI('statements/post').bind(this)
-		.then(function() {
-			return this.testDB('statements/post', 'database.statements', Statement);
-    })
-		.done(done, done);
-	});
-
-	it('handles GET /api/statements', function(done) {
-		this.setupDB('statements/get', 'database.statements', Statement).bind(this)
-		.then(function() {
-			return this.testAPI('statements/get');
-		})
-		.done(done, done);
-	});
-
-  it('handles POST /api/statements with tags', function(done) {
-    // TODO: things to discuss once this first test is passing:
-    //   - we must test when there are multiple tags in a statement.
-    //   - what happens when a tag with the same name already exists?
-    //   - what are the database constraints for tags?
-
-    // TODO: these won't use the same fixture
-    var fixtureName = 'statements/post-with-tag';
-    BPromise.resolve().bind(this)
-    .then(function() {
-      return this.testAPI(fixtureName);
-    })
-    .then(function() {
-      return this.testDB(fixtureName, 'database.statements', Statement);
-    })
-    .then(function() {
-      return this.testDB(fixtureName, 'database.tags', Tag);
-    })
-    .then(function() {
-      return this.testDB(fixtureName, 'database.statementsTags', StatementTag);
-    })
-    .done(done, done);
+  it('handles POST /api/statements', function(done) {
+    this.testAPI('statements/post').bind(this)
+      .then(function() {
+        return this.testDB('statements/post', 'database.statements', Statement);
+      })
+      .done(done, done);
   });
 
-  it('handles GET /api/tags', function(done) {
-    this.setupDB('tags/get-tag', 'database.tags', Tag).bind(this)
-    .then(function() {
-      return this.testAPI('tags/get-tag');
-    })
-    .done(done, done);
+  it('handles GET /api/statements', function(done) {
+    this.setupDB('statements/get', 'database.statements', Statement).bind(this)
+      .then(function() {
+        return this.testAPI('statements/get');
+      })
+      .done(done, done);
   });
 
-  it.skip('handles POST /api/statements with duplicate tags', function(done) {
-    // user submits statement with tag that is already in database
-    // statement post response:
-    // statement id, body, time and tag id, tag
-    // stored in database: statement id, body, time
-    // tag and tag id not added to tag table
-    // id, statement id, and tag id added to statement_it table
-    // TODO: discuss done!
-    var fixtureName = 'statements/post-duplicate-tags';
-    BPromise.bind(this)
-    .then(function() {
-      return this.setupDB(fixtureName,
-        'database.statements-existing', Statement);
-    })
-    .then(function() {
-      return this.setupDB(fixtureName,
-        'database.tags-existing', Tag);
-    })
-     .then(function() {
-       return this.setupDB(fixtureName,
-         'database.statementsTags-existing', StatementTag);
-     })
-    .then(function() {
-      return this.testAPI(fixtureName);
-    })
-    .then(function() {
-      return this.testDB(fixtureName, 'database.statements', Statement);
-    })
-    .then(function() {
-      return this.testDB(fixtureName, 'database.statementsTags', StatementTag);
-    })
-    .then(done)
-    .catch(done);
-  });
+  // it('handles POST /api/statements with tags', function(done) {
+  //   // TODO: things to discuss once this first test is passing:
+  //   //   - we must test when there are multiple tags in a statement.
+  //   //   - what happens when a tag with the same name already exists?
+  //   //   - what are the database constraints for tags?
 
-	describe('Captcha verification', function() {
-		var captchaApp = express();
-		var captchaServer;
+  //   // TODO: these won't use the same fixture
+  //   var fixtureName = 'statements/post-with-tag';
+  //   BPromise.resolve().bind(this)
+  //     .then(function() {
+  //       return this.testAPI(fixtureName);
+  //     })
+  //     .then(function() {
+  //       return this.testDB(fixtureName, 'database.statements', Statement);
+  //     })
+  //     .then(function() {
+  //       return this.testDB(fixtureName, 'database.tags', Tag);
+  //     })
+  //     .then(function() {
+  //       return this.testDB(fixtureName, 'database.statementsTags', StatementTag);
+  //     })
+  //     .done(done, done);
+  // });
 
-		captchaApp.post('/api', function(req, res) {
-			res.send('true\nsuccess');
-		});
+  // it('handles GET /api/tags', function(done) {
+  //   this.setupDB('tags/get-tag', 'database.tags', Tag).bind(this)
+  //     .then(function() {
+  //       return this.testAPI('tags/get-tag');
+  //     })
+  //     .done(done, done);
+  // });
 
-		beforeEach(function(done) {
-			captchaServer = captchaApp.listen(3333, done);
-		});
+  // it.skip('handles POST /api/statements with duplicate tags', function(done) {
+  //   // user submits statement with tag that is already in database
+  //   // statement post response:
+  //   // statement id, body, time and tag id, tag
+  //   // stored in database: statement id, body, time
+  //   // tag and tag id not added to tag table
+  //   // id, statement id, and tag id added to statement_it table
+  //   // TODO: discuss done!
+  //   var fixtureName = 'statements/post-duplicate-tags';
+  //   BPromise.bind(this)
+  //     .then(function() {
+  //       return this.setupDB(fixtureName,
+  //         'database.statements-existing', Statement);
+  //     })
+  //     .then(function() {
+  //       return this.setupDB(fixtureName,
+  //         'database.tags-existing', Tag);
+  //     })
+  //     .then(function() {
+  //       return this.setupDB(fixtureName,
+  //         'database.statementsTags-existing', StatementTag);
+  //     })
+  //     .then(function() {
+  //       return this.testAPI(fixtureName);
+  //     })
+  //     .then(function() {
+  //       return this.testDB(fixtureName, 'database.statements', Statement);
+  //     })
+  //     .then(function() {
+  //       return this.testDB(fixtureName, 'database.statementsTags', StatementTag);
+  //     })
+  //     .then(done)
+  //     .catch(done);
+  // });
 
-		afterEach(function(done) {
-			captchaServer.close(done);
-		});
+  // describe('Captcha verification', function() {
+  //   var captchaApp = express();
+  //   var captchaServer;
 
-		it('handles POST /api/captcha/verify', function(done) {
-			this.testAPI('captcha/post-verified')
-			.done(done, done);
-		});
-		// TODO: Add test for when Captcha fails.
-	});
+  //   captchaApp.post('/api', function(req, res) {
+  //     res.send('true\nsuccess');
+  //   });
+
+  //   beforeEach(function(done) {
+  //     captchaServer = captchaApp.listen(3333, done);
+  //   });
+
+  //   afterEach(function(done) {
+  //     captchaServer.close(done);
+  //   });
+
+  //   it('handles POST /api/captcha/verify', function(done) {
+  //     this.testAPI('captcha/post-verified')
+  //       .done(done, done);
+  //   });
+  //   // TODO: Add test for when Captcha fails.
+  // });
 }));
